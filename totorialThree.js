b2efe36307c81c5b79f6e185cfc45406ef954f5c
Greatness111 <111023667+Greatness111@users.noreply.github.com>
2023-08-12 16:05:37 +0100

    Create totorialThree.js
    Tutorial

---
+++ totorialThree.js
@@ -0,0 +1,13 @@
+let g = 1;
+let y = 2;
+let m = 3;
+let a = g + y;
+let b = a**2
+let d = a/a;
+let f = d**b;
+let h = 3 + m;
+let e = h/a;
+let k = e / m;
+let n = k * 4;
+let c = f - n;
+console.log(c);
